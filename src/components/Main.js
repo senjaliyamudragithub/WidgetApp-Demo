69890d32f6b5039152430a7786a684137c5a725d
senjaliyamudragithub <senjaliyamudra@yahoo.com>
2021-02-28 20:23:16 +0100

    modification

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,35 +4,32 @@ import Dashboard from "./Dashboard";
 import MultiWizard from "./MultiWizard";
 import { Route, Switch } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
+import ErrorBoundary from "./Errorboundary";
 
-const saveToLocal = (
-  widgetList = [
-    { language: "English", name: "Todo App", id: uuidv4() },
-    { language: "French", name: "Alarm", id: uuidv4() },
-    { language: "Spanish", name: "Random app", id: uuidv4() },
-    { language: "Deutsch", name: "Timer", id: uuidv4() },
-  ]
-) => {
+const saveToLocal = (widgetList) => {
   localStorage.setItem("widgetList", JSON.stringify(widgetList));
   return widgetList;
 };
 
 const Main = () => {
-  const [widgetList, setState] = React.useState([]);
+  const [widgetList, setState] = React.useState([
+    { language: "English", name: "Todo App", id: uuidv4() },
+    { language: "French", name: "Alarm", id: uuidv4() },
+    { language: "Spanish", name: "Random app", id: uuidv4() },
+    { language: "Deutsch", name: "Timer", id: uuidv4() },
+  ]);
   React.useEffect(() => {
-    const widgetList = saveToLocal();
-    setState(widgetList);
+    saveToLocal(widgetList);
     return () => {
       localStorage.removeItem("widgetList");
     };
-  }, []);
+  }, [widgetList]);
   const onDeleteWidget = ({ name, id }) => {
     setState((prevState) => {
       const modifiedList = prevState.filter(
         ({ name: matchingName, id: matchingId }) =>
           matchingName !== name && matchingId !== id
       );
-      saveToLocal(modifiedList);
       return modifiedList;
     });
   };
@@ -40,28 +37,29 @@ const Main = () => {
   const onAddWidget = (newWizard) => {
     setState((prevState) => {
       const modifiedList = [...prevState, { ...newWizard, id: uuidv4() }];
-      saveToLocal(modifiedList);
       return modifiedList;
     });
   };
   return (
     <>
-      <Navbar />
-      <Switch>
-        <Route
-          path="/addWidget"
-          render={() => <MultiWizard onAddWidget={onAddWidget} />}
-        />
-        <Route
-          path="/"
-          render={() => (
-            <Dashboard
-              widgetList={widgetList}
-              onDeleteWidget={onDeleteWidget}
-            />
-          )}
-        />
-      </Switch>
+      <ErrorBoundary>
+        <Navbar />
+        <Switch>
+          <Route
+            path="/addWidget"
+            render={() => <MultiWizard onAddWidget={onAddWidget} />}
+          />
+          <Route
+            path="/"
+            render={() => (
+              <Dashboard
+                widgetList={widgetList}
+                onDeleteWidget={onDeleteWidget}
+              />
+            )}
+          />
+        </Switch>
+      </ErrorBoundary>
     </>
   );
 };
---
+++ src/components/Main.test.js
@@ -0,0 +1,18 @@
+import Main from "./Main";
+import { createMemoryHistory } from "history";
+import { Router } from "react-router-dom";
+import { render, screen, cleanup } from "@testing-library/react";
+
+describe("RangeCounterB", () => {
+  afterEach(cleanup);
+  it("renders", () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={history}>
+        <Main />
+      </Router>
+    );
+    expect(screen.getByText(/Dashboard/)).toBeInTheDocument();
+    expect(screen.getByText(/Add Widget/)).toBeInTheDocument();
+  });
+});
